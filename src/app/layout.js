e53754c553251a81f09db148f9e21b05c78e7e93
Thanachote Hemchayart <75773822+BossThanachote@users.noreply.github.com>
2024-02-29 18:35:02 +0700

    Fix1

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "6404128",
-  description: "Thanacote Hemchayartssd",
+  description: "Thanacote HemchayartHalo",
 };
 
 export default function RootLayout({ children }) {
